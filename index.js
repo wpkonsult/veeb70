9f960fe21e0ff6dc86baa448787b35a5c5df6c78
andres.jarviste <andres.jarviste@gmail.com>
2021-02-25 12:55:27 +0200

    matkaandmed esilehele

--- index.js
+++ index.js
@@ -2,11 +2,39 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+let matkaAndmed1 = {
+  nimi: "Rabamatk",
+  kirjeldus: "Dlasdfld asdlfasdl falsdkfj aldkjfadfasdfjadlfkasdlf",
+  pilt: "./pildid/matkaja.png"
+}
+
+let matkaAndmed2 = {
+  nimi: "Rattamatk",
+  kirjeldus: "Väntame iga päev vähemalt 40 kilomeetrit. Ja nii nädal otsa",
+  pilt: "./pildid/rattamatk.jpg"
+}
+
+let matkaAndmed3 = {
+  nimi: "Süstamakt",
+  kirjeldus: "Sõidame iga päev vähemalt 10 kilomeetrit. Ja nii nädal otsa",
+}
+
+let koikMatkad = [
+  matkaAndmed1, 
+  matkaAndmed2, 
+  matkaAndmed3,
+  {
+      nimi: "Jalgsimatk",
+      kirjeldus: "Kõnnime iga päev vähemalt 10 kilomeetrit. Ja nii nädal otsa",
+      pilt: "./pildid/matk_tartus1.jpg"
+  }
+]
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', function(req, res) { return res.render('pages/index') })
+  .get('/', (req, res) => res.render('pages/index', {koikMatkad}))
   .get('/uudised', function(req, res) { return res.render('pages/uudised') })
   .get('/kontakt', function(req, res) { return res.render('pages/kontakt') })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
