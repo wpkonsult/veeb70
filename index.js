32d9be1c5afa850663e4d90e5a02c86772b5ddde
andres.jarviste <andres.jarviste@gmail.com>
2021-03-09 11:02:57 +0200

    mongo

--- index.js
+++ index.js
@@ -1,51 +1,59 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+const MongoClient = require("mongodb").MongoClient;
+const salas6na = "KalaSuppG0";
+const andmebaas = "veebmtest"; // <= Pane X asemel siia enda number
+const uri = `mongodb+srv://veebg0:${salas6na}@cluster0.qz3rv.mongodb.net/${andmebaas}?retryWrites=true&w=majority`;
 
 let matkaAndmed1 = {
   nimi: "Rabamatk",
   kirjeldus: "Dlasdfld asdlfasdl falsdkfj aldkjfadfasdfjadlfkasdlf",
   pilt: "/pildid/matkaja.png",
-  registreerunud: []
-}
+  registreerunud: [],
+};
 
 let matkaAndmed2 = {
   nimi: "Rattamatk",
   kirjeldus: "Väntame iga päev vähemalt 40 kilomeetrit. Ja nii nädal otsa",
   pilt: "/pildid/rattamatk.jpg",
-  registreerunud: []
-}
+  registreerunud: [],
+};
 
 let matkaAndmed3 = {
   nimi: "Süstamakt",
   kirjeldus: "Sõidame iga päev vähemalt 10 kilomeetrit. Ja nii nädal otsa",
-  registreerunud: []
-}
+  registreerunud: [],
+};
 
 let koikMatkad = [
-  matkaAndmed1, 
-  matkaAndmed2, 
+  matkaAndmed1,
+  matkaAndmed2,
   matkaAndmed3,
   {
-      nimi: "Jalgsimatk",
-      kirjeldus: "Kõnnime iga päev vähemalt 10 kilomeetrit. Ja nii nädal otsa",
-      pilt: "/pildid/matk_tartus1.jpg",
-      registreerunud: []
-  }
-]
+    nimi: "Jalgsimatk",
+    kirjeldus: "Kõnnime iga päev vähemalt 10 kilomeetrit. Ja nii nädal otsa",
+    pilt: "/pildid/matk_tartus1.jpg",
+    registreerunud: [],
+  },
+];
 
 function millineParameeter(req, res) {
   const testNumber = req.params.number;
-  res.send('Olen saanud testnumbri ' + testNumber)
+  res.send("Olen saanud testnumbri " + testNumber);
 }
 
 function matkaleRegistreerumine(req, res) {
-  const valitudMatkaIndeks = req.params.matk
-  const valitudMatkaAndmed = koikMatkad[valitudMatkaIndeks]
+  const valitudMatkaIndeks = req.params.matk;
+  const valitudMatkaAndmed = koikMatkad[valitudMatkaIndeks];
   if (valitudMatkaIndeks >= 0 && valitudMatkaIndeks < koikMatkad.length) {
-    return res.render('pages/registreerimine', {matk: valitudMatkaAndmed, matkaIndeks: valitudMatkaIndeks})
+    return res.render("pages/registreerimine", {
+      matk: valitudMatkaAndmed,
+      matkaIndeks: valitudMatkaIndeks,
+    });
   } else {
-    return res.render('pages/index', {koikMatkad})
+    return res.render("pages/index", { koikMatkad });
   }
 }
 
@@ -53,46 +61,94 @@ function lisaMatkaja(req, res) {
   const registreerunu = {
     nimi: req.query.nimi,
     email: req.query.email,
-  }
+  };
 
-  const matkaIndeks = parseInt(req.query.matk)
+  const matkaIndeks = parseInt(req.query.matk);
 
-  if ( matkaIndeks < 0 || matkaIndeks >= koikMatkad.length) {
+  if (matkaIndeks < 0 || matkaIndeks >= koikMatkad.length) {
     return res.send(`Viga: matka indeks ${matkaIndeks} on vigane`);
   }
-  
 
-  const valitudMatk = koikMatkad[matkaIndeks]
-  valitudMatk.registreerunud.push(registreerunu)
-  
-  console.log('Lisatud matkaja:')
-  console.log(valitudMatk)
+  const valitudMatk = koikMatkad[matkaIndeks];
+  valitudMatk.registreerunud.push(registreerunu);
 
-  return res.render(
-    'pages/kinnitus', 
-    { matk: valitudMatk, isikNimi: registreerunu.nimi }
-  );
-  
+  registreerunu.matk = matkaIndeks;
+
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  client.connect((err) => {
+    if (err) {
+      res.send({ error: "Viga: " + err.message });
+    } else {
+      const collection = client
+        .db(andmebaas)
+        .collection("matkaklubi_" + andmebaas + "_registreerumised");
+      collection.insertOne(registreerunu, (err) => {
+        client.close();
+        if (err) {
+          return res.send({ error: "Viga andmete lisamisel: " + err.message });
+        }
+
+        console.log("Lisatud matkaja:");
+        console.log(valitudMatk);
+      
+        return res.render("pages/kinnitus", {
+          matk: valitudMatk,
+          isikNimi: registreerunu.nimi,
+        });
+      });
+    }
+  });
 }
 
 function matkaleRegistreerunud(req, res) {
-  const matkaIndeks = req.params.matk
-  if ( matkaIndeks < 0 || matkaIndeks >= koikMatkad.length) {
-    return res.send({error: 'Matka indeks ' + matkaIndeks + ' ei ole õige'});
+  const matkaIndeks = req.params.matk;
+  if (matkaIndeks < 0 || matkaIndeks >= koikMatkad.length) {
+    return res.send({ error: "Matka indeks " + matkaIndeks + " ei ole õige" });
   }
-  return res.send(koikMatkad[matkaIndeks]);
+
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  client.connect((err) => {
+      if (err) {
+          return res.send({ error: 'Viga: ' + err.message });
+      } 
+      const collection = client
+        .db(andmebaas)
+        .collection("matkaklubi_" + andmebaas + "_registreerumised");
+      collection.find( {matk: 0}).toArray( 
+        (err, registreerumised) => {
+          client.close();
+          console.log(registreerumised)
+          if (err) {
+            return res.send({ error: 'Viga: ' + err.message });
+          }
+
+          koikMatkad[matkaIndeks].registreerunud = registreerumised
+          return res.send(koikMatkad[matkaIndeks]);
+      });
+
+  })
+ 
 }
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index', {koikMatkad}))
-  .get('/uudised', function(req, res) { return res.render('pages/uudised') })
-  .get('/kontakt', function(req, res) { return res.render('pages/kontakt') })
-  .get('/registreerimine', function(req, res) { return res.render('pages/registreeru') })
-  .get('/registreerimine/:matk', matkaleRegistreerumine)
-  .get('/testnumber/:number', millineParameeter)
-  .get('/lisaMatkaja', lisaMatkaja)
-  .get('/api/matkajad/:matk', matkaleRegistreerunud)
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index", { koikMatkad }))
+  .get("/uudised", function (req, res) {
+    return res.render("pages/uudised");
+  })
+  .get("/kontakt", function (req, res) {
+    return res.render("pages/kontakt");
+  })
+  .get("/registreerimine", function (req, res) {
+    return res.render("pages/registreeru");
+  })
+  .get("/registreerimine/:matk", matkaleRegistreerumine)
+  .get("/testnumber/:number", millineParameeter)
+  .get("/lisaMatkaja", lisaMatkaja)
+  .get("/api/matkajad/:matk", matkaleRegistreerunud)
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
